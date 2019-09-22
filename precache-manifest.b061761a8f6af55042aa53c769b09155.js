d8f1aa254121ce424e6078d2345cedb4e2c3ecbd
farhajhussain44 <farhajhussain29@gmail.com>
2019-09-22 22:25:15 +0530

    Updates

---
+++ precache-manifest.b061761a8f6af55042aa53c769b09155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f8f0cd76cb91c7cbdb5268b4b2ebda",
+    "url": "/farhajkhan/index.html"
+  },
+  {
+    "revision": "fb7a753f4bb19135adae",
+    "url": "/farhajkhan/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f0f1bbfcd0b684423882",
+    "url": "/farhajkhan/static/js/2.7c9ac4e9.chunk.js"
+  },
+  {
+    "revision": "fb7a753f4bb19135adae",
+    "url": "/farhajkhan/static/js/main.a006befe.chunk.js"
+  },
+  {
+    "revision": "efcbbcf1712083de00e0",
+    "url": "/farhajkhan/static/js/runtime-main.5f7eb546.js"
+  }
+]);
